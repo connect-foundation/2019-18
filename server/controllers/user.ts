8178381fe840f9759de668b3ca87900cda366280
Junow <taristmas@gmail.com>
2019-11-18 22:45:02 +0900

    [Add] 회원가입 api
    회원가입시 제출하는 email의 패턴 검사를 express-validator 모듈을 이용해 처리함

--- server/controllers/user.ts
+++ server/controllers/user.ts
@@ -1,8 +1,14 @@
 import { Request, Response } from 'express';
 import { create, remove } from '../services/user';
 
+const { validationResult } = require('express-validator');
 
 const signup = async (req: Request, res: Response) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ success: false, message: '올바른 이메일 형식이 아닙니다.' });
+  }
+
   const data = {
     ...req.body,
   };
@@ -12,10 +18,7 @@ const signup = async (req: Request, res: Response) => {
     return res.json(user);
   } catch (e) {
     if (e.name === 'MongoError') {
-      return res.json({ success: false, message: 'email is duplicated' });
-    }
-    if (e.name === 'ValidationError') {
-      return res.json({ success: false, message: e.errors.email.message });
+      return res.json({ success: false, message: '이미 사용중인 이메일입니다.' });
     }
     return res.json({ success: false, message: 'unknown error' });
   }
