cfdefa156e1c5ce5b09aebf35dbe446fa83b3490
슈우웅 <suphreme@gmail.com>
2019-11-18 20:13:40 +0900

    [Chore] lint 적용을 통한 코드 정리

--- server/controllers/oauth.ts
+++ server/controllers/oauth.ts
@@ -1,31 +1,27 @@
-import { Request, Response, NextFunction } from "express";
-import { oauth } from "../config/config";
-//import { loginService } from "../services/oauth";
+import { Request, Response, NextFunction } from 'express';
 
-const client_id = `${process.env.clientID}`;
-const client_secret = `${process.env.clientSECRET}`;
-const state = "RAMDOM_STATE";
-const redirectURI = encodeURI("http://106.10.58.138/oauth/callback");
+const clientId = `${process.env.clientID}`;
+const clientSecret = `${process.env.clientSECRET}`;
+const state = 'RAMDOM_STATE';
+const redirectURI = encodeURI('http://106.10.58.138/oauth/callback');
 
 const oauth = async (req: Request, res: Response, next: NextFunction) => {
-  const api_url =
-    "https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=" +
-    client_id +
-    "&redirect_uri=" +
-    redirectURI +
-    "&state=" +
-    state;
-  console.log(api_url);
-  res.writeHead(200, { "Content-Type": "text/html;charset=utf-8" });
+  const apiUrl = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${
+    clientId
+  }&redirect_uri=${
+    redirectURI
+  }&state=${
+    state}`;
+  res.writeHead(200, { 'Content-Type': 'text/html;charset=utf-8' });
   res.end(
-    "<a href='" +
-      api_url +
-      "'><img height='50' src='http://static.nid.naver.com/oauth/small_g_in.PNG'/></a>"
+    `<a href='${
+      apiUrl
+    }'><img height='50' src='http://static.nid.naver.com/oauth/small_g_in.PNG'/></a>`,
   );
 };
 const oauthCallback = async (
   req: Request,
   res: Response,
-  next: NextFunction
+  next: NextFunction,
 ) => {};
 export { oauth, oauthCallback };
