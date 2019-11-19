808df988cf9ba41b853c009ab0cf87f150c8d6a3
슈우웅 <suphreme@gmail.com>
2019-11-19 17:06:34 +0900

    [feat] access토큰으로, 현재 로그인한 사용자 정보 반환받고 로그인
    access 토큰이 정상적으로 반환되면, 해당하는 계정이 이미 있을 경우
    로그인 서비스를 활용하여 로그인함 (로그인 컨트롤러와 똑같이 행동)
    계정이 없을 경우 계정 생성 후 위와 동일하게 로그인 처리함

--- server/controllers/oauth.ts
+++ server/controllers/oauth.ts
@@ -1,9 +1,14 @@
 import { Request, Response, NextFunction } from 'express';
 import fetch from 'node-fetch';
+import { SSL_OP_NETSCAPE_CA_DN_BUG } from 'constants';
+import { isExist, create } from '../models/user';
+import { loginService } from '../services/login';
 
 const clientId = `${process.env.clientID}`;
 const clientSecret = `${process.env.clientSECRET}`;
 const redirectURI = encodeURI('http://106.10.58.138:3050/oauth/callback');
+const oauthCallBackURI = 'https://nid.naver.com/oauth2.0/token?grant_type=authorization_code&client_id=';
+const getInfoURI = 'https://openapi.naver.com/v1/nid/me';
 
 const oauth = async (_req: Request, res: Response, _next: NextFunction) => {
   const state = 'RAMDOM_STATE';
@@ -24,11 +29,16 @@ const oauth = async (_req: Request, res: Response, _next: NextFunction) => {
 const oauthCallback = async (
   req: Request,
   res: Response,
-  _next: NextFunction,
+  next: NextFunction,
 ) => {
   const { code } = req.query;
   const { state } = req.query;
-  const apiUrl = `https://nid.naver.com/oauth2.0/token?grant_type=authorization_code&client_id=${
+
+  // getAccessToken
+
+  const apiUrl = `${
+    oauthCallBackURI
+  }${
     clientId
   }&client_secret=${
     clientSecret
@@ -39,16 +49,57 @@ const oauthCallback = async (
   }&state=${
     state}`;
 
-  fetch(apiUrl, {
+  const responseToken = fetch(apiUrl, {
+    method: 'GET',
     headers: { 'X-Naver-Client-Id': clientId, 'X-Naver-Client-Secret': clientSecret },
-  })
-    .then((response) => {
-      if (response.status === 200) {
-        response.json().then((json) => res.json(json));
-      } else {
-        res.status(response.status).end();
-        console.log(`error = ${response.status}`);
-      }
+  });
+
+  if (responseToken.status !== '200') {
+    next(new Error('auth Error : accessToken 반환 실패'));
+  }
+
+  const tokenJson = await responseToken.json();
+  const accessToken = tokenJson.access_token;
+
+  // getUserInfo
+
+  const headers = { Authorization: `Bearer ${accessToken}` };
+  const responseUserInfo = await fetch(getInfoURI, {
+    method: 'GET',
+    headers,
+  });
+
+  if (responseUserInfo.status !== '200') {
+    next(new Error('auth Error : 사용자 정보 반환 실패'));
+  }
+
+  const infoJson = await responseUserInfo.json();
+
+  const {
+    name, email, profile_image, id,
+  } = infoJson;
+
+  const profileImage = profile_image;
+  // id : 네이버 아이디가 아닌, 사용자별 식별가능한 어떤 값
+  const pwd = id;
+
+
+  // 계정 생성
+  if (!await isExist(email)) {
+    const result = await create({
+      name, email, pwd, profile_image,
     });
+  }
+
+  // 로그인 처리
+  try {
+    const result = await loginService(email, id);
+    if (result.token !== null) {
+      res.cookie('token', result.token);
+    }
+    return res.status(result.status).json({ token: result.token, msg: result.msg });
+  } catch (e) {
+    next(e);
+  }
 };
 export { oauth, oauthCallback };
