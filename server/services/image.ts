9d8a932b1fb52fe63c64d24a12cfc206cba6c6c1
Junow <taristmas@gmail.com>
2019-11-20 22:18:44 +0900

    [Modified] getImages api에서 응답하는 filed 값 변경
    모든필드 -> creator, url

--- server/services/image.ts
+++ server/services/image.ts
@@ -1,6 +1,6 @@
 import Image from '../models/image';
 
-const get10Images = (skip, limit) => Image.find().skip(skip).limit(limit);
+const get10Images = (skip, limit) => Image.find().select({ creator: 1, url: 1 }).skip(skip).limit(limit);
 
 export {
   get10Images,
