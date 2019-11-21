71c09a6b4812fb3fc4f332940e174858dabc0b91
슈우웅 <suphreme@gmail.com>
2019-11-21 13:23:32 +0900

    [feat] 로그인 페이지용 네이버 아이콘 추가

--- client/src/components/Login/index.tsx
+++ client/src/components/Login/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import LoginInput from '../../basics/Input/LoginInput';
 import SubmitButton from '../../basics/Button/SubmitButton';
-
+import Img from '../../basics/Img';
+import NaverIcon from '../../assets/naver_login_white.png';
 
 interface LoginProp{
     onSubmit: (e: React.MouseEvent<HTMLButtonElement>)=>void;
@@ -16,6 +17,7 @@ const Login:React.FC<LoginProp> = ({
   <div>
     <LoginInput />
     <SubmitButton>로그인</SubmitButton>
+    <Img src={NaverIcon} />
     <input onChange={onChangeid} />
     <input onChange={onChangepwd} />
     <button type="button" onClick={onSubmit}>LOGIN</button>
