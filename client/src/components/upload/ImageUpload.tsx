d88191e6dee7bd347b64808a38dc1b7a65dc9fb5
jominjimail <jominjimail@gmail.com>
2019-11-21 16:09:41 +0900

    [Feat] 업로드 api #73 /upload 이미지와 글씨번갈아 가면서 쓴는 이미지 업로드 API 구현 contents object[] 이고 배열안에는 { type: string, content: string} 형식의 object 가 들어있음

--- client/src/components/upload/ImageUpload.tsx
+++ client/src/components/upload/ImageUpload.tsx
@@ -3,13 +3,22 @@ import React, {
 } from 'react';
 import ImageUploader from 'react-images-upload';
 import axios from 'axios';
+import uuidv4 from 'uuid/v4';
 import Preview from './Preview';
+
+interface ContentObject {
+  type: string,
+  content: string,
+}
 
 function ImageUpload() {
   const [pictures, setPictures] = useState <File[]>([]);
   const [previews, setPreviews] = useState<string[]>([]);
+  const [contents, setContents] = useState<ContentObject[]>([]);
+  const naniContent: Array<ContentObject> = [];
+
   useEffect(() => {
-    console.dir(pictures);
+
   });
   const onDrop = (picture: File[]) => {
     const newpicture = picture[picture.length - 1];
@@ -21,16 +30,44 @@ function ImageUpload() {
     setPictures(temp);
     const newUrls = temp.map((file) => URL.createObjectURL(file));
     setPreviews(newUrls);
+    const obj:ContentObject = {
+      type: 'image',
+      content: '',
+    };
+    naniContent.push(obj);
+    const temp2:ContentObject[] = contents;
+    temp2.push(obj);
+    setContents(temp2);
   };
 
-  const onClickHandler = () => {
-    const data = new FormData();
-    data.append('file', pictures[0]);
-    axios.post('http://localhost:3050/upload', data, { // receive two parameter endpoint url ,form data
-    })
-      .then((res) => { // then print response status
-        console.log(res.statusText);
-      });
+  const onClickHandler = async () => {
+    const urls = await getImageUrl();
+    console.log(contents);
+
+    const temp3 = contents.map((element2) => {
+      if (element2.type === 'image') {
+        element2.content = urls.shift();
+      }
+      return element2;
+    });
+    console.log(temp3);
+  };
+
+  const getImageUrl = async () => {
+    const formdata = new FormData();
+    pictures.forEach((element) => {
+      let format;
+      if (element.type === 'image/jpeg') {
+        format = '.jpg';
+      } else if (element.type === 'image/png') {
+        format = '.png';
+      }
+      const nameUuid = uuidv4();
+      formdata.append('multi-files', element, nameUuid + format);
+    });
+    const { data } = await axios.post('http://localhost:3050/upload', formdata);
+    const { objectStorageUrls } = data;
+    return objectStorageUrls;
   };
 
   const customButton = {
@@ -47,6 +84,16 @@ function ImageUpload() {
     border: '2px solid palevioletred',
   };
 
+  const addDescription: ()=> void = () => {
+    const obj:ContentObject = {
+      type: 'description',
+      content: '아무말 아무말',
+    };
+    const temp2:ContentObject[] = contents;
+    temp2.push(obj);
+    setContents(temp2);
+  };
+
   return (
     <div className="ImageUpload-container">
       <div>
@@ -59,7 +106,7 @@ function ImageUpload() {
           withLabel={false}
           buttonText="이미지 선택"
           onChange={onDrop}
-          imgExtension={['.jpg', '.gif', '.png', '.gif']}
+          imgExtension={['.jpg', '.png', '.gif']}
           maxFileSize={5242880}
           withPreview
           buttonStyles={customButton}
@@ -67,6 +114,7 @@ function ImageUpload() {
           singleImage
         />
       </div>
+      <button onClick={addDescription}>글씨 추가</button>
     </div>
   );
 }
