ded1636914372c570bc6fdfcf5c09b3d7ac29aab
Junow <taristmas@gmail.com>
2019-11-23 17:17:13 +0900

    [Modified] 유저 인터페이스 수정
    thumbnailUrl, originUrl을 optional 로 수정함.

--- server/interfaces/user.ts
+++ server/interfaces/user.ts
@@ -2,6 +2,6 @@ export interface IUser{
     email: string,
     pwd: string,
     name: string,
-    thumbnailUrl: string;
-    originUrl: string;
+    thumbnailUrl?: string;
+    originUrl?: string;
 }
