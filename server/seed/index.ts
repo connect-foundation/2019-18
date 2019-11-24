1865f7a5f710fb0e05f974ca7be86efcfa424e87
Junow <taristmas@gmail.com>
2019-11-24 20:42:49 +0900

    [Add] 데이터 seed 전 초기화 하도록 함

--- server/seed/index.ts
+++ server/seed/index.ts
@@ -10,12 +10,12 @@ const basename = path.basename(__filename);
 
 connect();
 fs.readdirSync(path.join(__dirname))
-  .forEach((file) => {
+  .forEach(async (file) => {
     if (file !== basename) {
       const data = require(path.join(__dirname, file)).default;
       const base = path.basename(file);
       const model = require(path.join(__dirname, '../models', base)).default;
-
+      await model.deleteMany({});
       model.create(data, (err) => {
         console.error('error: ', err);
         process.exit(0);
