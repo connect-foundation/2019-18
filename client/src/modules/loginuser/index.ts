b5dfe761d3fbc34d51994137903cb895fbfc1ba6
슈우웅 <suphreme@gmail.com>
2019-11-26 22:33:05 +0900

    [feature] makeUserState함수 구현
    서버로부터 쿠키에 저장된 토큰을 검증받고, 사용자 정보를
    반환받아, 사용자 정보를 리듀스의 state타입에 맞는 객체로 만들어
    준다

--- client/src/modules/loginuser/index.ts
+++ client/src/modules/loginuser/index.ts
@@ -1,14 +1,34 @@
 import { LoginUserState } from '../login/types';
+import { API_SERVER } from '../../utils/constants';
 
-export const makeUserState = (cookies:string) => {
-  const tempState = {
-    email: '',
-    name: '',
-    thumbnailUrl: '',
-    originUrl: '',
-    isLogin: false,
+const initialState = {
+  email: '',
+  name: '',
+  thumbnailUrl: '',
+  originUrl: '',
+  isLogin: false,
+};
+export const makeUserState = async (cookies:string) => {
+  const response = await fetch(`${API_SERVER}/login/whoAmI`, {
+    method: 'get',
+    credentials: 'include',
+  });
+
+  if (!response.ok) {
+    return initialState;
+  }
+
+  const { user } = (await response.json()).data;
+
+  const UserState = {
+    email: user.email,
+    name: user.name,
+    thumbnailUrl: user.thumbnailUrl,
+    originUrl: user.originUrl,
+    isLogin: true,
   };
-  return tempState;
+
+  return UserState;
 };
 export const setUserState = (LoginUser: LoginUserState) => {
   console.log(LoginUser);
