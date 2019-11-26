ca653e47d3e8cf99f18f69f29f52e17a16a93061
Junow <taristmas@gmail.com>
2019-11-27 04:12:32 +0900

    [Add] work-detail 디자인 추가

--- client/src/components/WorkDetail/index.tsx
+++ client/src/components/WorkDetail/index.tsx
@@ -3,6 +3,8 @@ import { RouteComponentProps } from 'react-router-dom';
 import { useParams } from 'react-router';
 import useGetFeed from '../../hooks/useGetFeed';
 import { API_SERVER } from '../../utils/constants';
+import Like from '../../commons/Like';
+
 import * as S from './styles';
 
 interface IData {
@@ -41,6 +43,29 @@ const WorkDetail = ({ match }: RouteComponentProps<{id:string}>) => {
         <S.Container>
           <S.Title>{data.title}</S.Title>
           <S.Creator>{`by ${data.owner.name} | 2019.11.26 | 조회${data.views}`}</S.Creator>
+
+          <S.CommentContainer>
+            <S.CommentHeader>내 아이디 올 자리</S.CommentHeader>
+            <S.CommentInput contentEditable></S.CommentInput>
+            <S.CommentFooter>
+              <S.Mention>멘션</S.Mention>
+              <S.Mention>비밀댓글</S.Mention>
+              <S.SubmitButton>등록</S.SubmitButton>
+            </S.CommentFooter>
+          </S.CommentContainer>
+
+          <S.Comment>
+            <S.CommentOwner>오너다</S.CommentOwner>
+            {data.comments[0]}
+            <S.CommentTimestamp>2019-07-10 09:01</S.CommentTimestamp>
+            <S.Right>
+              <Like initCount={10} />
+            </S.Right>
+          </S.Comment>
+          <S.Comment>{data.comments[1]}</S.Comment>
+
+
+          <S.CopyRight>{`Copyright © ${data.owner.name} All Rights Reserved`}</S.CopyRight>
         </S.Container>
 
       )
--- client/src/components/WorkDetail/styles.ts
+++ client/src/components/WorkDetail/styles.ts
@@ -1,13 +1,18 @@
 import styled from 'styled-components';
 import H3 from '../../basics/H3';
 import H4 from '../../basics/H4';
+import { theme } from '../../style/theme';
+import Textarea from '../../basics/Textarea';
+import Button from '../../basics/Button';
 
 export const Container = styled.div`
     width: 60%;
     display: flex;
     flex-direction: column;
     justify-content: center;
-    margin: auto
+    margin: auto;
+    padding: 1rem;
+    background: ${theme.MINT_LEAF};
 `;
 
 export const Title = styled(H3)`
@@ -16,4 +21,71 @@ export const Title = styled(H3)`
 
 export const Creator = styled(H4)`
     margin-right: auto;
+`;
+
+export const CopyRight = styled.div`
+    margin-right:auto;
+`;
+
+export const CommentContainer = styled.div`
+    width: auto;
+    max-height: fit-content;
+    min-height: 5rem;
+    border: 1px solid black;
+    padding: 1rem;
+    background: ${theme.CITY_LIGHT};
+`;
+
+export const CommentHeader = styled.div`
+    width: 100%;
+    background: ${theme.CHI_CONG};
+`;
+
+export const CommentInput = styled(Textarea)`
+    display: inline-block;
+    width: -webkit-fill-available; 
+    height: 5rem;
+    background: ${theme.CRA_YELLOW};
+`;
+
+export const CommentFooter = styled.div`
+    display: flex;
+    width: 100%;
+    height: fit-content;
+    background: ${theme.EXODUS_FRUIT};
+    align-items: center;
+`;
+
+export const SubmitButton = styled(Button)`
+    flex: none;
+    margin-left: auto;
+    border-radius:0;
+    height: 3rem;
+    width: 5rem;
+`;
+
+export const Mention = styled.span`
+    flex: none;
+    margin-right: 1rem;
+`;
+
+export const Comment = styled.div`
+    width: 100%;
+    height: fit-content;
+    background: ${theme.FIRST_DATA};
+    margin: 0.5rem auto;
+`;
+
+export const CommentOwner = styled.div`
+    background: ${theme.ROBINS_EGG_BLUE};
+`;
+
+export const CommentTimestamp = styled.span`
+    display: block;
+    font-size: 0.8rem;
+`;
+
+export const Right = styled.div`
+    display: flex;
+    justify-content: flex-end;
 `;
