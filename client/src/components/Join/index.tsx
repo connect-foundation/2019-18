a903dad45e4c6093a31f92deebaa4f20c7ddc141
슈우웅 <suphreme@gmail.com>
2019-11-27 16:21:15 +0900

    [Chore] 콘솔 로그 파일 없애기

--- client/src/components/Join/index.tsx
+++ client/src/components/Join/index.tsx
@@ -29,22 +29,19 @@ interface JoinProp{
 
 const Login:React.FC<JoinProp> = ({
   onJoin, onChangename, onChangepwdcheck, onChangepwd, onChangeemail, name, pwd, email, pwdcheck, joinSuccess,
-}) => {
-  console.log(joinSuccess);
-  return (
-    (joinSuccess.result)
-      ? (<Redirect to="/" />)
-      : (
-        <S.JoinBox>
-          <S.CrafolioLogoContainer>
-            <S.CrafolioLogo src={CrafolioIcon} />
-          </S.CrafolioLogoContainer>
-          <LoginInput onChange={onChangeemail} value={email} placeholder="이메일을 입력하세요" />
-          <LoginInput onChange={onChangename} value={name} placeholder="이름을 입력하세요" />
-          <PasswordInput onChange={onChangepwd} value={pwd} placeholder="비밀번호를 입력하세요" />
-          <PasswordInput onChange={onChangepwdcheck} value={pwdcheck} placeholder="비밀번호를 다시 입력하세요" />
-          <SubmitButton onClick={onJoin}>제출</SubmitButton>
-        </S.JoinBox>
-      ));
-};
+}) => (
+  (joinSuccess.result)
+    ? (<Redirect to="/login" />)
+    : (
+      <S.JoinBox>
+        <S.CrafolioLogoContainer>
+          <S.CrafolioLogo src={CrafolioIcon} />
+        </S.CrafolioLogoContainer>
+        <LoginInput onChange={onChangeemail} value={email} placeholder="이메일을 입력하세요" />
+        <LoginInput onChange={onChangename} value={name} placeholder="이름을 입력하세요" />
+        <PasswordInput onChange={onChangepwd} value={pwd} placeholder="비밀번호를 입력하세요" />
+        <PasswordInput onChange={onChangepwdcheck} value={pwdcheck} placeholder="비밀번호를 다시 입력하세요" />
+        <SubmitButton onClick={onJoin}>제출</SubmitButton>
+      </S.JoinBox>
+    ));
 export default Login;
