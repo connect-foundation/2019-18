8ea07feb870fa4b6e4decd8b70cad9e2ae58da76
슈우웅 <suphreme@gmail.com>
2019-11-28 13:03:15 +0900

    [chore] Oauth관련 상수 클라이언트에 추가

--- client/src/utils/constants.ts
+++ client/src/utils/constants.ts
@@ -7,7 +7,7 @@ export const API_SERVER:string = process.env.NODE_ENV === 'production'
 // OAUTH URL
 const CLIENT_ID = `${process.env.REACT_APP_clientID}`;
 const OAUTH_REDIRECT_URL:string = `${API_SERVER}/oauth/callback`;
-const STATE = 'RANDOMSTATE';
+const STATE = 'RAMDOM_STATE';
 
 export const OAUTH_URL:string = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${
   CLIENT_ID
