faa1c626d703e4c890bff74c100b12c032ea3cdb
Junow <taristmas@gmail.com>
2019-11-29 10:34:51 +0900

    [Chore] 코드 리뷰를 위해 불필요한 코드 삭제

--- server/customType/user.ts
+++
@@ -1,6 +0,0 @@
-export type User = {
-    name : string;
-    email : string;
-    thumbnailUrl : string;
-    originUrl: string;
-}
--- server/routes/upload.route.ts
+++
@@ -1,8 +0,0 @@
-import { getUrl, uploadWorkImage } from '../controllers/upload';
-import { multerUpload } from '../middleware/multer';
-
-const router = require('express').Router();
-
-router.post('/getImageUrl', multerUpload, getUrl);
-router.post('/works-image', uploadWorkImage);
-export default router;
