604aa12a3152b8d72ffb3a0cfb1e0b5fd27d0197
Junow <taristmas@gmail.com>
2019-12-02 02:55:53 +0900

    [Feat] 음악 재생, 일시정지 기능만 완성

--- client/src/components/FeedMusics/index.tsx
+++ client/src/components/FeedMusics/index.tsx
@@ -1,9 +1,52 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import * as S from './styles';
 
-const FeedMusics: React.FC = () => (
-  <div>
-    feed music;
-  </div>
-);
+const url = 'https://kr.object.ncloudstorage.com/crafolio/music/Happy_Haunts.mp3';
+
+const FeedMusics: React.FC = () => {
+  const [duration, setDuration] = useState(0);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [curTime, setCurTime] = useState(0);
+  const [volume, setVolume] = useState(0.5);
+
+
+  useEffect(() => {
+    const audio = document.getElementById('myaudio') as HTMLAudioElement;
+
+    const play = () => {
+      audio.play();
+      setIsPlaying(true);
+    };
+
+    const pause = () => {
+      audio.pause();
+      setIsPlaying(false);
+    };
+    audio.addEventListener('loadeddata', () => setDuration(audio.duration));
+    audio.addEventListener('timeupdate', () => setCurTime(audio.currentTime));
+    audio.addEventListener('canplaythrough', () => {
+      document.getElementById('play-button')!.addEventListener('click', play);
+      document.getElementById('pause-button')!.addEventListener('click', pause);
+    });
+    audio.volume = volume;
+  }, []);
+  return (
+    <div>
+      <audio
+        id="myaudio"
+        // controls
+        src="https://kr.object.ncloudstorage.com/crafolio/music/Happy_Haunts.mp3"
+      />
+
+      <p>{duration}</p>
+      <p>{curTime}</p>
+      <button type="button" id="play-button">play</button>
+      <button type="button" id="pause-button">pause</button>
+
+    </div>
+
+
+  );
+};
 
 export default FeedMusics;
---
+++ client/src/components/FeedMusics/styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    display: flex;
+`;
+
+export const AudioPlayer = styled.div`
+    margin: 0 audo;
+    
+`;
