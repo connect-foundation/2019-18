68703a37e963be8a489e43cf3053b5b8efafd628
Junow <taristmas@gmail.com>
2019-12-04 02:56:22 +0900

    [Add] 새로운 글꼴 추가 - Bebas Neue

--- client/src/style/globalStyle.ts
+++ client/src/style/globalStyle.ts
@@ -2,22 +2,44 @@ import { createGlobalStyle } from 'styled-components';
 import { theme } from './theme';
 import RobotoLightWoff from './fonts/RobotoLight.woff';
 import RobotoLightWoff2 from './fonts/RobotoLight.woff2';
+import NoteworthyLightWoff from './fonts/NoteworthyLight.woff';
+import NoteworthyLightWoff2 from './fonts/NoteworthyLight.woff2';
+import BebasNeueRegular from './fonts/BebasNeueRegular.ttf';
 
 const GlobalStyle = createGlobalStyle`
+  @font-face {
+    font-family: 'Roboto Light';
+    src: local('Roboto Light'), local('RobotoLight'),
+    url(${RobotoLightWoff}) format('woff'),
+    url(${RobotoLightWoff2}) format('woff2');
+    font-weight: 300;
+    font-style: normal;
+  }
+
+  @font-face {
+    font-family: 'Noteworthy Light';
+    src: local('Noteworthy Light'), local('NoteworthyLight'),
+    url(${NoteworthyLightWoff}) format('woff'),
+    url(${NoteworthyLightWoff2}) format('woff2');
+    font-weight: 300;
+    font-style: normal;
+  }
+
+  @font-face {
+    font-family: 'Bebas Neue';
+    src: local('Bebas Neue'), local('Bebas Neue'),
+    url(${BebasNeueRegular}) format('truetype');
+    font-weight: 300;
+    font-style: normal;
+  }
+
   body{
     padding: 0;
     margin: 0;
     background: ${theme.background};
-    font-family: 'Roboto Light';
+    font-family: 'Bebas Neue', "NoteWorthy Light", 'Roboto Light';
   }
-  @font-face {
-    font-family: 'Roboto Light';
-    src: local('Roboto Light'), local('RobotoLight'),
-    url(${RobotoLightWoff}) format(woff),
-    url(${RobotoLightWoff2}) format(woff2);
-    font-weight: 300;
-    font-style: normal;
-  }
+
 `;
 
 export default GlobalStyle;
