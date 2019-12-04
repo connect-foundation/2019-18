18963db96c7a7ee9d16139b1dd3df4d35162abfb
슈우웅 <suphreme@gmail.com>
2019-12-04 10:45:28 +0900

    [fix]  app.tsx 에서 로직 제거
    준호님의 의견을 따라 app.tsx에서의 로직을 제거함
    쿠키에서 사용자 정보를 셋하는 로직을
    home/index.tsx를로 이동

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,20 +1,21 @@
 import React from 'react';
 import { createGlobalStyle } from 'styled-components';
-import { ReactCookieProps, withCookies } from 'react-cookie';
 import dotenv from 'dotenv';
 import { ThemeProvider } from './style/typed-compoennts';
 // import {SetUserContainer} from './containers/SetUserContainer';
 import { theme } from './style/theme';
 import Home from './components/Home';
-import useUserState from './hooks/useUserState';
 
 dotenv.config();
 const GlobalStyle = createGlobalStyle`
+  html{
+    height : 100%;
+  }
   body{
     padding: 0;
     margin: 0;
     background: ${theme.background};
-
+    height: 100%;
     @font-face {
       font-family: 'Anton Regular';
       src: url('./style/fonts/Anton-Regular.woff') format('woff'); /* Modern Browsers */
@@ -24,16 +25,18 @@ const GlobalStyle = createGlobalStyle`
     }
     font-family: "Anton Regular", sans-serif;
   }
+  div#root{
+    height: 100%;
+    display:flex;
+    flex-direction: column;
+  }
 `;
 
-const App:React.FC<ReactCookieProps> = (props:ReactCookieProps) => {
-  useUserState(props);
-  return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <Home />
-    </ThemeProvider>
-  );
-};
+const App:React.FC = () => (
+  <ThemeProvider theme={theme}>
+    <GlobalStyle />
+    <Home />
+  </ThemeProvider>
+);
 
-export default withCookies(App);
+export default App;
