f6a42e94c6ac0cd93b6f8f95509a77bc63f9dfc9
슈우웅 <suphreme@gmail.com>
2019-12-04 21:31:29 +0900

    [feature] 포토폴리오 폼, 값 서버에서 받아와서 초기화
    useEffect를 사용 맨 처음에만 데이터를 로딩해온다

--- client/src/containers/PortfolioFormContainer.tsx
+++ client/src/containers/PortfolioFormContainer.tsx
@@ -1,8 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
+import { optionCSS } from 'react-select/src/components/Option';
 import PortfolioForm from '../components/PortfolioForm';
 import { theme } from '../style/theme';
-import { fieldoptions } from '../utils/constants';
+import { fieldoptions, API_SERVER } from '../utils/constants';
 
 const S = {
   PortfolioFormContainer: styled.div`
@@ -13,11 +14,38 @@ const S = {
 };
 const makeFirstStates = () => fieldoptions.map((option) => ({ ...option, checked: false }));
 
+
 const PortfolioFormContainer:React.FC = () => {
   const [introSimple, setIntroSimple] = useState('');
   const [introDetail, setIntroDetail] = useState('');
   const [activeField, setActiveFields] = useState(makeFirstStates);
   const [showOption, setShowOption] = useState(false);
+  useEffect(() => {
+    const getData = async () => {
+      const response = await fetch(`${API_SERVER}/profile`, {
+        method: 'get',
+        credentials: 'include',
+        headers: { 'Content-Type': 'application/json' },
+      });
+      const responseJson = await response.json();
+      if (!responseJson.success) {
+        return null;
+      }
+      return responseJson.data;
+    };
+    const setForm = (result:any) => {
+      if (!result) {
+        return;
+      }
+      setIntroSimple(result.introSimple);
+      setIntroDetail(result.introDetail);
+      setActiveFields(activeField.map((option) => {
+        if (result.activeFields.includes(option.value)) { return { ...option, checked: true }; }
+        return option;
+      }));
+    };
+    getData().then((result) => setForm(result));
+  }, []);
 
   const onChangeintroSimple = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     setIntroSimple(e.target.value);
