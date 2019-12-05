da4766cb696a74c8e429d545433602a1192801da
Junow <taristmas@gmail.com>
2019-12-05 19:23:01 +0900

    [Chore] 코드정리

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,17 @@
 import React from 'react';
-import { createGlobalStyle } from 'styled-components';
-import { ReactCookieProps, withCookies } from 'react-cookie';
 import dotenv from 'dotenv';
 import { ThemeProvider } from './style/typed-compoennts';
 import { theme } from './style/theme';
 import Home from './components/Home';
-import useUserState from './hooks/useUserState';
 import GlobalStyle from './style/globalStyle';
 
 dotenv.config();
 
-const App:React.FC = (props) => {
-  useUserState(props);
-
-  return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <Home />
-    </ThemeProvider>
-  );
-};
+const App:React.FC = (props) => (
+  <ThemeProvider theme={theme}>
+    <GlobalStyle />
+    <Home />
+  </ThemeProvider>
+);
 
 export default App;
