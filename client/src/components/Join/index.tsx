71e5f4105d32f3ff2efc6ff684b5a3e9d82c2097
슈우웅 <suphreme@gmail.com>
2019-12-05 16:45:19 +0900

    [refactor] join type.ts에 타입 빼기

--- client/src/components/Join/index.tsx
+++ client/src/components/Join/index.tsx
@@ -6,26 +6,11 @@ import CrafolioIcon from '../../assets/logo_white_larger.png';
 import LoginInput from '../../basics/Input/LoginInput';
 import PasswordInput from '../../basics/Input/PasswordInput';
 import SubmitButton from '../../basics/SubmitButton';
-
-
-interface JoinProp{
-    onJoin: (e: React.MouseEvent<HTMLButtonElement>)=>void;
-    onChangename: (e: React.ChangeEvent<HTMLInputElement>)=> void;
-    onChangeemail: (e: React.ChangeEvent<HTMLInputElement>)=> void;
-    onChangepwdcheck: (e: React.ChangeEvent<HTMLInputElement>)=> void;
-    onChangepwd: (e: React.ChangeEvent<HTMLInputElement>)=> void;
-
-    pwd: string;
-    name: string;
-    email: string;
-    pwdcheck: string;
-    joinSuccess: boolean
-
-}
+import { JoinProp } from './types';
 
 
 const Join:React.FC<JoinProp> = ({
-  onJoin, onChangename, onChangepwdcheck, onChangepwd, onChangeemail, name, pwd, email, pwdcheck, joinSuccess,
+  onJoin, onChangeName, onChangePwdCheck, onChangePwd, onChangeEmail, name, pwd, email, pwdCheck, joinSuccess,
 }) => (
   (joinSuccess)
     ? (<Redirect to="/login" />)
@@ -34,10 +19,10 @@ const Join:React.FC<JoinProp> = ({
         <S.CrafolioLogoContainer to="/">
           <S.CrafolioLogo src={CrafolioIcon} />
         </S.CrafolioLogoContainer>
-        <LoginInput onChange={onChangeemail} value={email} placeholder="이메일을 입력하세요" />
-        <LoginInput onChange={onChangename} value={name} placeholder="이름을 입력하세요" />
-        <PasswordInput onChange={onChangepwd} value={pwd} placeholder="비밀번호를 입력하세요" />
-        <PasswordInput onChange={onChangepwdcheck} value={pwdcheck} placeholder="비밀번호를 다시 입력하세요" />
+        <LoginInput onChange={onChangeEmail} value={email} placeholder="이메일을 입력하세요" />
+        <LoginInput onChange={onChangeName} value={name} placeholder="이름을 입력하세요" />
+        <PasswordInput onChange={onChangePwd} value={pwd} placeholder="비밀번호를 입력하세요" />
+        <PasswordInput onChange={onChangePwdCheck} value={pwdCheck} placeholder="비밀번호를 다시 입력하세요" />
         <SubmitButton onClick={onJoin}>제출</SubmitButton>
       </S.Join>
     ));
