2f566e91a1d1dd1ed9e85d110ab090de03b5648c
Junow <taristmas@gmail.com>
2019-12-05 16:46:32 +0900

    [Merge]

--- server/routes/feed.route.ts
+++ server/routes/feed.route.ts
@@ -6,14 +6,9 @@ import jwt from '../middleware/jwt';
 const router = require('express').Router();
 
 router.get('/images', getImages);
-<<<<<<< HEAD
-router.get('/images/:id', getWorkImage);
 router.post('/images/:id/add-comment', jwt, addComment);
-=======
 router.get('/images/more/:fixedNum/:skippedNum', getMoreImages);
 router.get('/workimage/:id', getWorkImage);
-router.post('/images/:id/add-comment', addComment);
->>>>>>> b56116383d9dee7a64ec0f97559dc406558ce10c
 router.get('/wallpapers', getWallpapers);
 router.get('/wallpapers/more/:fixedNum/:skippedNum', getMoreWallpapers);
 export default router;
