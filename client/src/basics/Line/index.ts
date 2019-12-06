7c07ed1d3fbcd706354482dfe54f05f351b294e9
Junow <taristmas@gmail.com>
2019-12-06 10:48:37 +0900

    [Delete] 불필요한 코드 삭제

--- client/src/basics/Line/index.ts
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-
-const Line = styled.hr`
-    height: 2px;
-    border:none;
-`;
-
-export default Line;
--- server/customType/user.ts
+++
@@ -1,7 +0,0 @@
-export type User = {
-    _id: string;
-    name : string;
-    email : string;
-    thumbnailUrl : string;
-    originUrl: string;
-}
