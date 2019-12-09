f10fd0832c4b0b26c18ac52ab4bdabefa2c91955
슈우웅 <suphreme@gmail.com>
2019-12-09 22:45:16 +0900

    헤더 Alarm컴포넌트 디자인
    나름 정성

--- client/src/components/Header/HeaderRight/HeaderGreeting/styles.ts
+++ client/src/components/Header/HeaderRight/HeaderGreeting/styles.ts
@@ -4,19 +4,19 @@ import Img from '../../../../basics/Img';
 import Input from '../../../../basics/Input';
 
 export const HeaderSearchContainer = styled.div`
-display: flex;
-width: 15rem;
-height: 2rem;
-border-radius: 100px;
-border: none;
-background: #f3f3f3;
-margin: auto;
+    display: flex;
+    width: 15rem;
+    height: 2rem;
+    border-radius: 100px;
+    border: none;
+    background: #f3f3f3;
+    margin: auto;
 `;
 
 export const SearchImg = styled(Img)`
-width: 1rem;
-height: 1rem;
-margin: auto 1rem;
+    width: 1rem;
+    height: 1rem;
+    margin: auto 1rem;
 `;
 
 export const HeaderInput = styled(Input)`
