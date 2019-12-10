1a95379875d027a8ec3d60cc516796e1f6701200
Junow <taristmas@gmail.com>
2019-12-10 11:33:04 +0900

    [Add] 음악작품 피드 업로드하는 컨트롤러 추가

--- server/controllers/upload.ts
+++ server/controllers/upload.ts
@@ -2,8 +2,16 @@ import { Request, Response, NextFunction } from 'express';
 import mongoose from 'mongoose';
 import createError from 'http-errors';
 import httpStatus from 'http-status';
-import { workImageCreate, imageCreate, wallPaperCreate } from '../services/upload';
+import {
+  workImageCreate,
+  imageCreate,
+  wallPaperCreate,
+  createMusic,
+  createWorkMusic,
+} from '../services/upload';
 import { AUTH } from '../utils/messages';
+import { IMusicContent } from '../interfaces/workMusic';
+import response from '../utils/response';
 
 interface MulterFile {
     key: string,
@@ -76,4 +84,59 @@ const uploadWorkImage = async (req: Request, res:Response, next: NextFunction) =
   }
 };
 
-export { getUrl, uploadWorkImage };
+
+const uploadMusicFeed = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const user = req.decodedUser;
+    if (!user) {
+      throw (createError(httpStatus.UNAUTHORIZED, AUTH.UNAUTHORIZED));
+    }
+
+    const { title } = req.body;
+    const { content } = req.body;
+    console.dir(content);
+    const workMusicData = {
+      title,
+      content,
+      owner: user.id,
+      comments: [],
+      commentsAllow: true,
+      ccl: 'All Rights Reserved',
+      field: '음악',
+      public: true,
+      tags: [],
+      views: 0,
+    };
+    console.log('-------------data-------------');
+    console.log(workMusicData);
+
+    const newWorkMusic = await createWorkMusic(workMusicData);
+    newWorkMusic.content.forEach(async (newMusicContent) => {
+      if (newMusicContent.type === 'musics') {
+        const musicContent = newMusicContent.content as IMusicContent;
+
+        const newMusicData = {
+          owner: newWorkMusic.id,
+          creator: user.id,
+          public: true,
+          ref: [],
+          ...musicContent,
+        };
+
+        const newMusic = await createMusic(newMusicData);
+        console.log(newMusic);
+      } else {
+        throw (createError(httpStatus[400], '잘못된 요청입니다.'));
+      }
+    });
+
+    response(res, newWorkMusic);
+  } catch (e) {
+    next(e);
+  }
+};
+export {
+  getUrl,
+  uploadWorkImage,
+  uploadMusicFeed,
+};
