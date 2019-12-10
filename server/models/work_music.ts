7d7bcac1a62a5eb8481fd58737e201351302de99
Junow <taristmas@gmail.com>
2019-12-10 11:30:38 +0900

    [Add] 음악 작품 모델에 minimize 옵션 추가

--- server/models/work_music.ts
+++ server/models/work_music.ts
@@ -13,18 +13,21 @@ export interface IWorkMusicModel extends IWorkMusic, Document{}
 
 const workMusicSchema = new Schema({
   owner: {
-    type: ObjectId, required: true, unique: true, ref: User,
+    type: ObjectId, required: true, ref: User,
   },
   title: { type: String, required: true },
   content: { type: [Schema.Types.Mixed], required: true, default: [] },
   comments: { type: [Schema.Types.Mixed], required: true, default: [] },
   commentsAllow: { type: Boolean, required: true, default: true },
   ccl: { type: String, required: true },
-  field: { type: String, required: true },
+  field: { type: String, required: true, default: '' },
   public: { type: String, required: true, default: true },
   tags: { type: [String], required: true, default: [] },
   views: { type: Number, required: true, default: 0 },
-}, { timestamps: { createdAt: 'createdAt', updatedAt: 'updatedAt' } });
+}, {
+  timestamps: { createdAt: 'createdAt', updatedAt: 'updatedAt' },
+  minimize: false,
+});
 
 const WorkMusic:Model<IWorkMusicModel> = model<IWorkMusicModel>('WorkMusic', workMusicSchema);
 
