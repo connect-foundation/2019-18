5b8f076094e75f9fa5856a835fd0c48c548a90a7
슈우웅 <suphreme@gmail.com>
2019-12-11 13:56:40 +0900

    [fix] 빠뜨린 파일 추가

--- client/src/basics/emptyButton/index.ts
+++ client/src/basics/emptyButton/index.ts
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-const ReverseButton = styled.button`
+export const ReverseButton = styled.button`
     border: 2px ${(props) => props.theme.green} solid;
     border-radius: 10px;
     height: 2rem;
