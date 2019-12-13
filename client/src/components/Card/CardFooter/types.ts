50debc0d50c6b58117c0959ef32c9a29cec98e88
Junow <taristmas@gmail.com>
2019-12-13 11:02:07 +0900

    [Chore] 불필요한 파일 삭제

--- client/src/components/Card/CardFooter/types.ts
+++
@@ -1,5 +0,0 @@
-export default interface CardFooterProp {
-  smiles: string;
-  comments: string;
-  views: string;
-}
--- server/interfaces/music.ts
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-
-export default interface IMusic {
-  owner: mongoose.Types.ObjectId;
-  creator: mongoose.Types.ObjectId;
-  public : boolean;
-  ref: mongoose.Types.ObjectId[];
-  url: string;
-}
