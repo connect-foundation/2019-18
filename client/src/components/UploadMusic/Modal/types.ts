b714bc25d66c8ed97936e78c9b275931b4749944
Junow <taristmas@gmail.com>
2019-12-13 11:16:02 +0900

    [Chore] 불필요한 코드 삭제

--- client/src/components/UploadMusic/Modal/types.ts
+++
@@ -1,9 +0,0 @@
-export interface ModalProp {
-  lists: string[];
-  datas: string[];
-  changeHandler: (e:React.MouseEvent<HTMLLIElement>)=>void;
-}
-
-export interface LiProp {
-  selected?: boolean;
-}
