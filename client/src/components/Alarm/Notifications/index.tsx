77e8fd4116d2f1b2da842230f460c0d59455d0e8
Junow <taristmas@gmail.com>
2019-12-16 01:23:09 +0900

    [Add] 알림창에 데이터 가져오는 hook 추가

--- client/src/components/Alarm/Notifications/index.tsx
+++ client/src/components/Alarm/Notifications/index.tsx
@@ -1,14 +1,73 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import axios from 'axios';
 import * as S from './styles';
+import { API_SERVER } from '../../../utils/constants';
+import { RootState } from '../../../modules';
+import useFetch from '../../../hooks/useFetch';
+import { getShortId } from '../../../utils';
+
+interface INoti {
+  isRead: boolean;
+  createdAt: number;
+  _id: string;
+  sender: string;
+  ref: string;
+  type: string;
+}
+
+interface IData {
+  _id: string,
+  notifications:INoti[];
+}
 
 const Notifications: React.FC = () => {
-  const [notis, setNotis] = useState();
+  const user = useSelector((state:RootState) => state.login);
+  const {
+    data, isLoading, isError, setUrl,
+  } = useFetch<IData>();
+
+
+  useEffect(() => {
+    setUrl(`${API_SERVER}/user/notifications/${user.id}`);
+  }, []);
+
+  if (!data) {
+    return (
+      <S.Container>
+        <p>알림이 없습니다.</p>
+      </S.Container>
+    );
+  }
+
+  if (isLoading) {
+    return (
+      <S.Container>
+        <div>Loading...</div>
+      </S.Container>
+    );
+  }
+
+  if (isError) {
+    return (
+      <S.Container>
+        <div>something wrong...</div>
+      </S.Container>
+    );
+  }
 
   return (
-    <S.container>
-      <div>123</div>
-      <div>234</div>
-    </S.container>
+    <S.Container>
+      {
+        data.notifications.map((noti) => (
+          <div key={getShortId()}>
+            <div>{noti.sender}</div>
+            <div>{noti.ref}</div>
+            <div>{noti.type}</div>
+          </div>
+        ))
+      }
+    </S.Container>
   );
 };
 
--- client/src/components/Alarm/Notifications/styles.ts
+++ client/src/components/Alarm/Notifications/styles.ts
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-export const container = styled.div`
+export const Container = styled.div`
   display: flex;
   flex-direction: column;
   width: 10rem;
