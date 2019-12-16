ff0ed64de5efb9101d7f8169eddf4415d114b774
jominjimail <jominjimail@gmail.com>
2019-12-16 17:59:08 +0900

    [Refactory] 유저의의 profile을 가져오는 부분은 populate 로 변경
    mongo db 의 장점을 활용하는 방향으로 코드 개선

--- server/controllers/follow.ts
+++ server/controllers/follow.ts
@@ -1,26 +1,19 @@
 import {
-  findProfile, followingUpdate, followerUpdate, findProfilePopulate,
-} from '../services/profile';
-import { findById } from '../services/user';
-import { AUTH, PROFILE, USER, PARAMS } from '../utils/messages';
-import {
   Request, Response, NextFunction,
 } from 'express';
 import createError from 'http-errors';
 import httpStatus from 'http-status';
-import mongoose from 'mongoose';
 import {
-  findById, findFollower, findFollowing,
-} from '../services/user';
+  AUTH, PROFILE, USER, PARAMS,
+} from '../utils/messages';
+import { findById } from '../services/user';
+import {
+  findProfile, followingUpdate, followerUpdate, findProfileByUserId,
+} from '../services/profile';
+
 import response from '../utils/response';
 
 const addFollowing = async (req: Request, res: Response, next: NextFunction) => {
-  // dev 용
-  // const targetId = '5df671be1bf9dc2bb1e940c6';
-  // const user = {
-  //   profile: '5df676bf688d0e3471bdfa0a',
-  //   id: '5df676bf688d0e3471bdfa0b',
-  // };
   try {
     const user = req.decodedUser;
     if (!user) {
@@ -50,19 +43,13 @@ const addFollowing = async (req: Request, res: Response, next: NextFunction) => 
       user.id,
     ];
     await followerUpdate(targetProfile.id, newFollwer);
-    res.json({ success: true });
+    response(res);
   } catch (e) {
     next(e);
   }
 };
 
 const deleteFollowing = async (req: Request, res:Response, next:NextFunction) => {
-  // dev 용
-  // const targetId = '5df671d31bf9dc2bb1e940c8';
-  // const user = {
-  //   profile: '5df671be1bf9dc2bb1e940c5',
-  //   id: '5df671be1bf9dc2bb1e940c6',
-  // };
   try {
     const user = req.decodedUser;
     if (!user) {
@@ -86,79 +73,7 @@ const deleteFollowing = async (req: Request, res:Response, next:NextFunction) =>
     }
     const newFollwer = targetProfile.follower.filter((e) => !e.equals(user.id));
     await followerUpdate(targetProfile.id, newFollwer);
-    res.json({ success: true });
-  } catch (e) {
-    next(e);
-  }
-};
-
-const getAllFollow = async (req: Request, res: Response, next: NextFunction) => {
-  // const user = {
-  //   profile: '5df671be1bf9dc2bb1e940c5',
-  //   id: '5df671be1bf9dc2bb1e940c6',
-  // };
-  try {
-    const user = req.decodedUser;
-    if (!user) {
-      throw (createError(httpStatus.UNAUTHORIZED, AUTH.UNAUTHORIZED));
-    }
-    const profile = await findProfilePopulate(user.profile);
-    if (profile === null) {
-      throw Error(PROFILE.NOT_MATCH);
-    }
-    res.json({ profile });
-  } catch (e) {
-    next(e);
-  }
-};
-
-const getFollowers = async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    const { id } = req.params;
-    if (!id || !mongoose.Types.ObjectId.isValid(id)) {
-      throw (createError(httpStatus.BAD_REQUEST, PARAMS.NOT_CORRECT_PARAMS));
-    }
-    const result = await findFollower(id);
-    if (!result) {
-      throw (createError(httpStatus.NOT_FOUND, LOGIN.ID_NOT_MATCH));
-    }
-    const { follower } = result.profile! as any;
-    const filteredFollowers = follower.map((user) => {
-      const {
-        _id, email, name, thumbnailUrl, ...rest
-      } = user;
-
-      return {
-        _id, email, name, thumbnailUrl,
-      };
-    });
-    response(res, filteredFollowers);
-  } catch (e) {
-    next(e);
-  }
-};
-
-const getFollowing = async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    const { id } = req.params;
-    if (!id || !mongoose.Types.ObjectId.isValid(id)) {
-      throw (createError(httpStatus.BAD_REQUEST, PARAMS.NOT_CORRECT_PARAMS));
-    }
-    const result = await findFollowing(id);
-    if (!result) {
-      throw (createError(httpStatus.BAD_REQUEST, PARAMS.NOT_CORRECT_PARAMS));
-    }
-    const { following } = result.profile! as any;
-    const filteredFollowings = following.map((user) => {
-      const {
-        _id, email, name, thumbnailUrl, ...rest
-      } = user;
-
-      return {
-        _id, email, name, thumbnailUrl,
-      };
-    });
-    response(res, filteredFollowings);
+    response(res);
   } catch (e) {
     next(e);
   }
@@ -167,7 +82,4 @@ const getFollowing = async (req: Request, res: Response, next: NextFunction) => 
 export {
   addFollowing,
   deleteFollowing,
-  getAllFollow,
-  getFollowers,
-  getFollowing,
 };
