e03769a4c4187cdb3d8e50f2f80051515d4c65bc
jominjimail <jominjimail@gmail.com>
2019-12-17 14:15:15 +0900

    [Modified] MusicFeedPlayerMini를 추가로 만듦
    MusicPlayerMini 를 사용하고 있는 다른 코드가 있어서 일단
    이렇게 진행

--- client/src/components/Card/MusicCard/index.tsx
+++ client/src/components/Card/MusicCard/index.tsx
@@ -1,39 +1,52 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import * as S from './styles';
 import StyledLink from '../../../basics/StyledLink';
 import { TextWithImg } from '../CardFooter/styles';
 import CardFooter from '../CardFooter';
 import MusicCardProp from './types';
-import MusicPlayerMini from '../../MusicPlayerMini';
+import MusicFeedPlayerMini from '../../MusicFeedPlayerMini';
 
 const MusicCard: React.FC<MusicCardProp> = ({
   _id, imageUrl, musicUrl, title, creator, ownerId, numOfComments, views,
-}) => (
-  <S.Container>
-    <StyledLink to={`/home/detail-music/${ownerId}`}>
-      <S.CardImgContainer>
-        <S.CardImg src={imageUrl} />
-      </S.CardImgContainer>
+}) => {
+  const [isPlaying, setIsPlaying] = useState(false);
+  const audioRef = useRef<HTMLAudioElement>(null);
 
-      <S.CardHeader>
-        <S.Title>{title}</S.Title>
-      </S.CardHeader>
+  const playToggle = (e:React.MouseEvent<HTMLButtonElement>) => {
+    setIsPlaying(!isPlaying);
+  };
 
-      <S.CardBody id={creator._id}>
-        <TextWithImg
-          src="https://kr.object.ncloudstorage.com/crafolio/user/thumbnail/user-profile-thumbnail.png"
-          text={creator.name}
+  return (
+    <S.Container>
+      <StyledLink to={`/home/detail-music/${ownerId}`}>
+        <S.CardImgContainer>
+          <S.CardImg src={imageUrl} />
+
+        </S.CardImgContainer>
+
+        <S.CardHeader>
+          <S.Title>{title}</S.Title>
+        </S.CardHeader>
+
+        <S.CardBody id={creator._id}>
+          <TextWithImg
+            src="https://kr.object.ncloudstorage.com/crafolio/user/thumbnail/user-profile-thumbnail.png"
+            text={creator.name}
+          />
+        </S.CardBody>
+
+        <CardFooter
+          smiles="20"
+          comments={numOfComments.toString()}
+          views={views.toString()}
         />
-      </S.CardBody>
-
-      <CardFooter
-        smiles="20"
-        comments={numOfComments.toString()}
-        views={views.toString()}
-      />
-    </StyledLink>
-    <MusicPlayerMini url={musicUrl} />
-  </S.Container>
-);
+      </StyledLink>
+      <label htmlFor="playbutton">
+        <button type="button" id="playbutton" onClick={playToggle} />
+      </label>
+      <MusicFeedPlayerMini url={musicUrl} isPlaying={isPlaying} playToggle={playToggle} audioRef={audioRef} />
+    </S.Container>
+  );
+};
 
 export default MusicCard;
